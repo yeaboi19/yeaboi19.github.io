fffe304e48f5dc9b3035b21eb9af1d08353077bc
yeaboi19 <lolhaha544@gmail.com>
2021-11-26 17:35:45 +0400

    Update, didnt get margins

--- Me_When_The/WorkInProgress/Bruh.js
+++ Me_When_The/WorkInProgress/Bruh.js
@@ -1,12 +1,12 @@
 var col = 0;
 
 function changeColor() {
-    if (col == 0) {
-        document.getElementById("MainText").style.color = "green";
-        col = 1;
-    } else {
-        document.getElementById("MainText").style.color = "gold";
-        col = 0;
-    }
+  if (col == 0) {
+    document.getElementById("MainText").style.color = "green";
+    col = 1;
+  } else {
+    document.getElementById("MainText").style.color = "gold";
+    col = 0;
+  }
 }
 setInterval(changeColor, 500);
